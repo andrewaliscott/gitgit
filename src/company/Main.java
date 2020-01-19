62aabd01906352536e75e7aa8bede3c4cc4ae997
Andrew <scottandrewali@gmail.com>
2020-01-19 00:31:45 -0500

    There is a conflict

--- src/company/Main.java
+++ src/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("This is experimental");
         System.out.println("This is Experiment2");
         System.out.println("I did this in Boot Branch");
+        System.out.println("I did this in new branch exactly at the same line");
     }
 }
