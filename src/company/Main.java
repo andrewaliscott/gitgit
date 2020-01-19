d87698175f42a615d3897b63783d07ff27d621cd
Andrew <scottandrewali@gmail.com>
2020-01-19 00:15:54 -0500

    I added navbar branch, I changed things

--- src/company/Main.java
+++ src/company/Main.java
@@ -6,5 +6,6 @@ public class Main {
         System.out.println("This is experimental");
         System.out.println("This is Experiment2");
         System.out.println("I did this in Boot Branch");
+        System.out.println("I added navbar branch iam going to create a merge conflict");
     }
 }
